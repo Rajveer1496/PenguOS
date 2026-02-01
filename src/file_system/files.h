53e4af11e97296ef627e140f2ab76afbe86b7fa1
Rajveer Chaudhari <rajveer14961496@gmail.com>
2026-02-01 16:45:43 +0530

    File Metadata approaches
    Brain storming about different approaches

--- src/file_system/files.h
+++ src/file_system/files.h
@@ -14,5 +14,31 @@ extern int set_sector_inUse(uint32_t sectorNO);
 extern int check_sector_usage(uint32_t sectorNO);
 extern int set_sector_free(uint32_t sectorNO);
 
+//File metadata format
+struct file{
+    char name[100];
+    int size_bytes; //in bytes
+
+}
+
+//My solution #1
+/*
+How many sectors needed?
+store some sectors (for small files)
+store info of sector of sectors
+512 bytes -> (32 bit int) store info of 127 sectors (128th reserved for sector of sectors of sectors)
+if need more sectors
+store info of sector of sectors of sector at last 128th sector
+
+store sector info in range format (more efficient)
+[(1000,4), (1045,2), (2001,10)] -> (starting sector, no of continous sectors)
+*/
+
+/* SOLUTION 2
+How many sectors needed?
+how big will be sector info?
+create file for file to store sector info
+store info of sector info file
+*/
 
 #endif
